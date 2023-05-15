f44d0aba7791d2a36c5b1a3f9b95a4223835e23f
elibeam12 <elibeam024@gmail.com>
2023-05-14 23:00:02 -0500

    budtr

---
+++ app/src/main/java/com/example/budrt/TransactionDao.kt
@@ -0,0 +1,19 @@
+package com.example.budrt
+
+import androidx.room.*
+import com.example.budrt.Transaction
+
+@Dao
+interface TransactionDao {
+    @Query("SELECT * from transactions")
+    fun getAll(): List<Transaction>
+
+    @Insert
+    fun insertAll(vararg transaction: Transaction)
+
+    @Delete
+    fun delete(transaction: Transaction)
+
+    @Update
+    fun update(vararg transaction: Transaction)
+}
